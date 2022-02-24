6ea83d9dfb233f5aef4bab814b7ca3a3811b6bb4
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2022-02-24 18:29:10 +0800

    Update

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
 import React from "react";
 import World from "./src/World";
-import Difficulty from "./src/components/Difficulty";
+import DifficultyLevel from "./src/components/Difficulty";
 import { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const [DifficultySelected, selectedDifficulty] = useState("");
+  const [DifficultySelected, selectedDifficulty] = useState(1);
+  const [difficulty, setDifficulty] = useState(1);
+
+  useEffect(() => {
+  console.log(difficulty)
+  }, [difficulty]);
 
   function handleDifficultySelect(updateDifficulty) {
     selectedDifficulty(updateDifficulty);
   }
+  
   return (
-    <>
-      <Difficulty passToMain={handleDifficultySelect} selectedDifficulty={DifficultySelected} />
-      <World gravity= {DifficultySelected} />
-    </>
+    <View style={styles.container}>
+      <DifficultyLevel
+        difficultyValue={difficulty}
+        handleSetDifficulty={setDifficulty}
+      />
+      <World gravity={difficulty} />
+      </View>
   );
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea"
+  }})
 // export default App;
--- src/components/Difficulty.js
+++ src/components/Difficulty.js
@@ -2,35 +2,32 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import DropDownPicker from "react-native-dropdown-picker";
 
-function difficultyLevel(props) {
+function DifficultyLevel(props) {
   const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
+  
   const [items, setItems] = useState([
     { label: "Level 1", value: 1 },
     { label: "Level 2", value: 2 },
   ]);
-  useEffect(() => {}, []);
+  // useEffect(() => {}, []);
 
-  async function retrieveDifficultyLevel() {
-    return (
-      <DropDownPicker
-        open={open}
-        value={value}
-        items={items}
-        setOpen={setOpen}
-        setValue={handleDifficultySelect}
-        setItems={setItems}
-      />
-    );
-  }
+  
+  // function handleDifficultySelect(e) {
+  //   e.preventDefault();
+  //   props.passToMain(value);
+  //   props.selectedDifficulty(value);
+  // }
 
-  function handleDifficultySelect(e) {
-    e.preventDefault();
-    props.passToMain(value);
-    props.selectedDifficulty(value);
-  }
-
-  return <>{retrieveDifficultyLevel()}</>;
+  return (
+    <DropDownPicker
+      open={open}
+      value={props.difficultyValue}
+      items={items}
+      setOpen={setOpen}
+      setValue={props.handleSetDifficulty}
+      setItems={setItems}
+    />
+  );
 }
 
-export default difficultyLevel;
+export default DifficultyLevel;
